238216a02a1bd181c044ce768b15606dd339736e
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-15 06:52:14 +0200

    plugin for pgsql constraints (much simpler than mysql)

---
+++ src/Plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package Dbmover
+ * @subpackage Pgsql
+ * @subpackage Constraints
+ */
+
+namespace Dbmover\Pgsql\Constraints;
+
+use Dbmover\Constraints;
+
+class Plugin extends Constraints\Plugin
+{
+    protected function dropConstraint(string $table, string $constraint, string $type)
+    {
+        if (in_array($type, ['PRIMARY KEY', 'FOREIGN KEY'])) {
+            $this->addOperation("ALTER TABLE $table DROP CONSTRAINT IF EXISTS $constraint CASCADE;");
+        }
+    }
+}
+
